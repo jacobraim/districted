e1a7b0703ddb4b8f3159a64631bcf6ac76d9e671
jacobraim <jacob.raim@gmail.com>
2026-05-14 15:02:41 -0400

    Update puzzles.js

--- src/puzzles.js
+++ src/puzzles.js
@@ -1,16 +1,32 @@
 export const puzzlesByDate = {
+"2026-05-15": {
+  id: "001",
+  locations: [
+    {
+      name: "Meridian Hill Park",
+      category: "Park",
+      answer: { lat: 38.920822, lng: -77.035713 }
+    },
+    {
+      name: "Union Market",
+      category: "Food Hall",
+      answer: { lat: 38.904167, lng: -77.000278 }
+    },
+{ name: "National Theatre", category: "Theater", answer: { lat: 38.896339, lng: -77.030517 } },
+    {
+      name: "Florida Avenue Grill",
+      category: "Restaurant",
+      answer: { lat: 38.917488, lng: -77.026373 }
+    },
+    {
+      name: "The Wonderland Ballroom",
+      category: "Bar",
+      answer: { lat: 38.931991, lng: -77.032500 }
+    }
+  ]
+},
+  
   "2026-05-18": {
-    id: "001",
-    locations: [
-      { name: "The Lincoln Memorial", category: "Monument", answer: { lat: 38.889269, lng: -77.050176 } },
-      { name: "Ben's Chili Bowl", category: "Restaurant", answer: { lat: 38.917025, lng: -77.03145 } },
-      { name: "Nationals Park", category: "Sports", answer: { lat: 38.872987, lng: -77.007435 } },
-      { name: "The Anthem", category: "Music Venue", answer: { lat: 38.881954, lng: -77.026972 } },
-      { name: "Dan's Cafe", category: "Dive Bar", answer: { lat: 38.914139, lng: -77.042258 } }
-    ]
-  },
-
-  "2026-05-19": {
     id: "002",
     locations: [
       { name: "Union Market", category: "Food Hall", answer: { lat: 38.908635, lng: -76.997448 } },
@@ -21,10 +37,21 @@ export const puzzlesByDate = {
     ]
   },
 
-  "2026-05-20": {
+  "2026-05-19": {
     id: "003",
     locations: [
-      { name: "The Kennedy Center", category: "Performing Arts", answer: { lat: 38.895911, lng: -77.055889 } },
+      { name: "The Lincoln Memorial", category: "Monument", answer: { lat: 38.889269, lng: -77.050176 } },
+      { name: "Ben's Chili Bowl", category: "Restaurant", answer: { lat: 38.917025, lng: -77.03145 } },
+      { name: "Nationals Park", category: "Sports", answer: { lat: 38.872987, lng: -77.007435 } },
+      { name: "The Anthem", category: "Music Venue", answer: { lat: 38.881954, lng: -77.026972 } },
+      { name: "Dan's Cafe", category: "Dive Bar", answer: { lat: 38.914139, lng: -77.042258 } }
+    ]
+  },
+
+  "2026-05-20": {
+    id: "004",
+    locations: [
+{ name: "National Theatre", category: "Theater", answer: { lat: 38.896339, lng: -77.030517 } },
       { name: "Florida Avenue Grill", category: "Restaurant", answer: { lat: 38.917488, lng: -77.026373 } },
       { name: "Meridian Hill Park", category: "Park", answer: { lat: 38.920536, lng: -77.035902 } },
       { name: "Union Station", category: "Transit", answer: { lat: 38.897095, lng: -77.006332 } },
@@ -33,7 +60,7 @@ export const puzzlesByDate = {
   },
 
   "2026-05-21": {
-    id: "004",
+    id: "005",
     locations: [
       { name: "Rock Creek Park Nature Center", category: "Park", answer: { lat: 38.959006, lng: -77.052162 } },
       { name: "Supreme Court of the United States", category: "Civic Landmark", answer: { lat: 38.890642, lng: -77.004439 } },
@@ -44,7 +71,7 @@ export const puzzlesByDate = {
   },
 
   "2026-05-22": {
-    id: "005",
+    id: "006",
     locations: [
       { name: "Library of Congress", category: "Civic Landmark", answer: { lat: 38.888684, lng: -77.004719 } },
       { name: "Bluejacket", category: "Brewery", answer: { lat: 38.875333, lng: -77.000614 } },
