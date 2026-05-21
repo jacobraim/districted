0f5a097ca1db7b4b8b7a45a0be674f9be486b9fb
jacobraim <jacob.raim@gmail.com>
2026-05-21 15:46:21 -0400

    Update puzzles.js

--- src/puzzles.js
+++ src/puzzles.js
@@ -1,229 +1,249 @@
 export const puzzlesByDate = {
   "2026-05-19": {
     id: "001",
-      locations: [
-    {
-      name: "The Anthem",
-      category: "Music Venue",
-      answer: { lat: 38.8801, lng: -77.0260 }
-    },
-    {
-      name: "Washington Monument",
-      category: "Monument",
-      answer: { lat: 38.889484, lng: -77.035278 }
-    },
-    {
-      name: "National Zoo",
-      category: "Zoo",
-      answer: { lat: 38.930000, lng: -77.049000 }
-    },
-    {
-      name: "Renwick Gallery",
-      category: "Museum",
-      answer: { lat: 38.899140, lng: -77.039032 }
-    },
-    {
-      name: "Gemini",
-      category: "Restaurant",
-      answer: { lat: 38.9101, lng: -77.0383 }
-    }
-  ]
+    locations: [
+      {
+        name: "The Anthem",
+        category: "Music Venue",
+        answer: { lat: 38.8801, lng: -77.0260 }
+      },
+      {
+        name: "Washington Monument",
+        category: "Monument",
+        answer: { lat: 38.889484, lng: -77.035278 }
+      },
+      {
+        name: "National Zoo",
+        category: "Zoo",
+        answer: { lat: 38.930000, lng: -77.049000 }
+      },
+      {
+        name: "Renwick Gallery",
+        category: "Museum",
+        answer: { lat: 38.899140, lng: -77.039032 }
+      },
+      {
+        name: "Gemini",
+        category: "Restaurant",
+        answer: { lat: 38.9101, lng: -77.0383 }
+      }
+    ]
   },
 
   "2026-05-21": {
     id: "002",
     locations: [
-    {
-      name: "Hirshhorn Museum",
-      category: "Museum",
-      answer: { lat: 38.88813, lng: -77.02292 }
-    },
-    {
-      name: "Audi Field",
-      category: "Stadium",
-      answer: { lat: 38.868408, lng: -77.012878 }
-    },
-    {
-      name: "Exorcist Stairs",
-      category: "Landmark",
-      answer: { lat: 38.90563, lng: -77.07018 }
-    },
-    {
-      name: "Howard University",
-      category: "University",
-      answer: { lat: 38.919700, lng: -77.018700 }
-    },
-    {
-      name: "Capital One Arena",
-      category: "Arena",
-      answer: { lat: 38.898129, lng: -77.021172 }
-    }
-  ]
+      {
+        name: "Hirshhorn Museum",
+        category: "Museum",
+        answer: { lat: 38.88813, lng: -77.02292 }
+      },
+      {
+        name: "Audi Field",
+        category: "Stadium",
+        answer: { lat: 38.868408, lng: -77.012878 }
+      },
+      {
+        name: "Exorcist Stairs",
+        category: "Landmark",
+        answer: { lat: 38.90563, lng: -77.07018 }
+      },
+      {
+        name: "Howard University",
+        category: "University",
+        answer: { lat: 38.919700, lng: -77.018700 }
+      },
+      {
+        name: "Capital One Arena",
+        category: "Arena",
+        answer: { lat: 38.898129, lng: -77.021172 }
+      }
+    ]
   },
 
   "2026-05-22": {
     id: "003",
     locations: [
-      { name: "Thompson Boat Center at the Georgetown Waterfront", category: "Park", answer: { lat: 38.9029, lng: -77.0653 } },
-      { name: "9:30 Club", category: "Music Venue", answer: { lat: 38.9179480, lng: -77.0237227 } },
-      { name: "The Lincoln Memorial", category: "Monument", answer: { lat: 38.889248, lng: -77.050636 } },
-      { name: "Metrobar", category: "Restaurant and Bar", answer: { lat: 38.9217946, lng: -76.9977994 } },
-      { name: "United States Botanic Garden", category: "Landmark", answer: { lat: 38.8881, lng: -77.0129 } }
+      {
+        name: "Thompson Boat Center at the Georgetown Waterfront",
+        category: "Park",
+        answer: { lat: 38.9029, lng: -77.0653 }
+      },
+      {
+        name: "9:30 Club",
+        category: "Music Venue",
+        answer: { lat: 38.9179480, lng: -77.0237227 }
+      },
+      {
+        name: "The Lincoln Memorial",
+        category: "Monument",
+        answer: { lat: 38.889248, lng: -77.050636 }
+      },
+      {
+        name: "Metrobar",
+        category: "Restaurant and Bar",
+        answer: { lat: 38.9217946, lng: -76.9977994 }
+      },
+      {
+        name: "United States Botanic Garden",
+        category: "Landmark",
+        answer: { lat: 38.8881, lng: -77.0129 }
+      }
     ]
-  }
-};
+  },
 
   "2026-05-25": {
     id: "004",
     locations: [
-     {
-      name: "YELLOW Georgetown",
-      category: "Cafe",
-      answer: { lat: 38.9095, lng: -77.06444 }
-    },
-    {
-      name: "Soleluna",
-      category: "Bakery",
-      answer: { lat: 38.92301, lng: -77.04217 }
-    },
-    {
-      name: "Banneker Recreation Center",
-      category: "Recreation Center",
-      answer: { lat: 38.92272, lng: -77.02231 }
-    },
-    {
-      name: "Hillwood Estate, Museum and Gardens",
-      category: "Museum",
-      answer: { lat: 38.94377, lng: -77.05267 }
-    },
-    {
-      name: "Hirshhorn Museum",
-      category: "Museum",
-      answer: { lat: 38.88757, lng: -77.02208 }
-    }
-  ]
-};
+      {
+        name: "YELLOW Georgetown",
+        category: "Cafe",
+        answer: { lat: 38.9095, lng: -77.06444 }
+      },
+      {
+        name: "Soleluna",
+        category: "Bakery",
+        answer: { lat: 38.92301, lng: -77.04217 }
+      },
+      {
+        name: "Banneker Recreation Center",
+        category: "Recreation Center",
+        answer: { lat: 38.92272, lng: -77.02231 }
+      },
+      {
+        name: "Hillwood Estate, Museum and Gardens",
+        category: "Museum",
+        answer: { lat: 38.94377, lng: -77.05267 }
+      },
+      {
+        name: "Hirshhorn Museum",
+        category: "Museum",
+        answer: { lat: 38.88757, lng: -77.02208 }
+      }
+    ]
+  },
 
   "2026-05-26": {
     id: "005",
-  locations: [
-    {
-      name: "Albi",
-      category: "Restaurant",
-      answer: { lat: 38.88473, lng: -77.000267 }
-    },
-    {
-      name: "National Building Museum",
-      category: "Museum",
-      answer: { lat: 38.89813, lng: -77.01753 }
-    },
-    {
-      name: "Meridian Hill Park",
-      category: "Park",
-      answer: { lat: 38.921551, lng: -77.035762 }
-    },
-    {
-      name: "Black Cat",
-      category: "Music Venue",
-      answer: { lat: 38.91457, lng: -77.03195 }
-    },
-    {
-      name: "Dumbarton Oaks",
-      category: "Museum/Garden",
-      answer: { lat: 38.913338, lng: -77.063089 }
-    }
-  ]
-};
+    locations: [
+      {
+        name: "Albi",
+        category: "Restaurant",
+        answer: { lat: 38.88473, lng: -77.000267 }
+      },
+      {
+        name: "National Building Museum",
+        category: "Museum",
+        answer: { lat: 38.89813, lng: -77.01753 }
+      },
+      {
+        name: "Meridian Hill Park",
+        category: "Park",
+        answer: { lat: 38.921551, lng: -77.035762 }
+      },
+      {
+        name: "Black Cat",
+        category: "Music Venue",
+        answer: { lat: 38.91457, lng: -77.03195 }
+      },
+      {
+        name: "Dumbarton Oaks",
+        category: "Museum/Garden",
+        answer: { lat: 38.913338, lng: -77.063089 }
+      }
+    ]
+  },
 
   "2026-05-27": {
     id: "006",
     locations: [
-    {
-      name: "Le Diplomate",
-      category: "Restaurant",
-      answer: { lat: 38.91136, lng: -77.03195 }
-    },
-    {
-      name: "National Portrait Gallery",
-      category: "Museum",
-      answer: { lat: 38.897875, lng: -77.022898 }
-    },
-    {
-      name: "Yards Park",
-      category: "Park",
-      answer: { lat: 38.873452, lng: -77.001116 }
-    },
-    {
-      name: "Politics and Prose Bookstore",
-      category: "Bookstore",
-      answer: { lat: 38.95524, lng: -77.06952 }
-    },
-    {
-      name: "Folger Shakespeare Library",
-      category: "Library/Theater",
-      answer: { lat: 38.88982, lng: -77.00277 }
-    }
-  ]
-};
+      {
+        name: "Le Diplomate",
+        category: "Restaurant",
+        answer: { lat: 38.91136, lng: -77.03195 }
+      },
+      {
+        name: "National Portrait Gallery",
+        category: "Museum",
+        answer: { lat: 38.897875, lng: -77.022898 }
+      },
+      {
+        name: "Yards Park",
+        category: "Park",
+        answer: { lat: 38.873452, lng: -77.001116 }
+      },
+      {
+        name: "Politics and Prose Bookstore",
+        category: "Bookstore",
+        answer: { lat: 38.95524, lng: -77.06952 }
+      },
+      {
+        name: "Folger Shakespeare Library",
+        category: "Library/Theater",
+        answer: { lat: 38.88982, lng: -77.00277 }
+      }
+    ]
+  },
 
   "2026-05-28": {
     id: "007",
-  locations: [
-    {
-      name: "L'Ardente",
-      category: "Restaurant",
-      answer: { lat: 38.89943, lng: -77.01411 }
-    },
-    {
-      name: "National Cathedral",
-      category: "Cathedral",
-      answer: { lat: 38.93097, lng: -77.07096 }
-    },
-    {
-      name: "Hains Point",
-      category: "Park",
-      answer: { lat: 38.856840, lng: -77.022138 }
-    },
-    {
-      name: "Union Stage",
-      category: "Music Venue",
-      answer: { lat: 38.87853, lng: -77.02378 }
-    },
-    {
-      name: "The Mansion on O Street",
-      category: "Museum/Hotel",
-      answer: { lat: 38.90857, lng: -77.04585 }
-    }
-  ]
-};
+    locations: [
+      {
+        name: "L'Ardente",
+        category: "Restaurant",
+        answer: { lat: 38.89943, lng: -77.01411 }
+      },
+      {
+        name: "National Cathedral",
+        category: "Cathedral",
+        answer: { lat: 38.93097, lng: -77.07096 }
+      },
+      {
+        name: "Hains Point",
+        category: "Park",
+        answer: { lat: 38.856840, lng: -77.022138 }
+      },
+      {
+        name: "Union Stage",
+        category: "Music Venue",
+        answer: { lat: 38.87853, lng: -77.02378 }
+      },
+      {
+        name: "The Mansion on O Street",
+        category: "Museum/Hotel",
+        answer: { lat: 38.90857, lng: -77.04585 }
+      }
+    ]
+  },
 
   "2026-05-29": {
     id: "008",
-  locations: [
-    {
-      name: "Capital One Arena",
-      category: "Arena",
-      answer: { lat: 38.89778, lng: -77.020898 }
-    },
-    {
-      name: "Nationals Park",
-      category: "Stadium",
-      answer: { lat: 38.872672, lng: -77.007483 }
-    },
-    {
-      name: "Kenilworth Aquatic Gardens",
-      category: "Garden",
-      answer: { lat: 38.91183, lng: -76.94087 }
-    },
-    {
-      name: "Arena Stage",
-      category: "Theater",
-      answer: { lat: 38.87713, lng: -77.01987 }
-    },
-    {
-      name: "Oyster Oyster",
-      category: "Restaurant",
-      answer: { lat: 38.90922, lng: -77.02295 }
-    }
-  ]
+    locations: [
+      {
+        name: "Capital One Arena",
+        category: "Arena",
+        answer: { lat: 38.89778, lng: -77.020898 }
+      },
+      {
+        name: "Nationals Park",
+        category: "Stadium",
+        answer: { lat: 38.872672, lng: -77.007483 }
+      },
+      {
+        name: "Kenilworth Aquatic Gardens",
+        category: "Garden",
+        answer: { lat: 38.91183, lng: -76.94087 }
+      },
+      {
+        name: "Arena Stage",
+        category: "Theater",
+        answer: { lat: 38.87713, lng: -77.01987 }
+      },
+      {
+        name: "Oyster Oyster",
+        category: "Restaurant",
+        answer: { lat: 38.90922, lng: -77.02295 }
+      }
+    ]
+  }
 };
