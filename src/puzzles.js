11f0cd54c8fa2ae15411af25f1604569ebadde9a
jacobraim <jacob.raim@gmail.com>
2026-05-21 15:37:12 -0400

    Update puzzles.js

--- src/puzzles.js
+++ src/puzzles.js
@@ -1,53 +1,4 @@
 export const puzzlesByDate = {
-"2026-05-14": {
-  id: "001",
-  locations: [
-    {
-      name: "Meridian Hill Park",
-      category: "Park",
-      answer: { lat: 38.920822, lng: -77.035713 }
-    },
-    {
-      name: "Union Market",
-      category: "Food Hall",
-      answer: { lat: 38.904167, lng: -77.000278 }
-    },
-{ name: "National Theatre", category: "Theater", answer: { lat: 38.896339, lng: -77.030517 } },
-    {
-      name: "Florida Avenue Grill",
-      category: "Restaurant",
-      answer: { lat: 38.917488, lng: -77.026373 }
-    },
-    {
-      name: "The Wonderland Ballroom",
-      category: "Bar",
-      answer: { lat: 38.931991, lng: -77.032500 }
-    }
-  ]
-},
-
-    "2026-05-15": {
-    id: "002",
-    locations: [
-      { name: "Union Market", category: "Food Hall", answer: { lat: 38.908635, lng: -76.997448 } },
-      { name: "The Phillips Collection", category: "Museum", answer: { lat: 38.911971, lng: -77.046739 } },
-      { name: "Eastern Market", category: "Market", answer: { lat: 38.884316, lng: -76.995659 } },
-      { name: "9:30 Club", category: "Music Venue", answer: { lat: 38.917999, lng: -77.023739 } },
-      { name: "Gravelly Point", category: "Park", answer: { lat: 38.864456, lng: -77.043419 } }
-    ]
-  },
-  
-  "2026-05-18": {
-    id: "003",
-    locations: [
-      { name: "Union Market", category: "Food Hall", answer: { lat: 38.908635, lng: -76.997448 } },
-      { name: "The Phillips Collection", category: "Museum", answer: { lat: 38.911971, lng: -77.046739 } },
-      { name: "Eastern Market", category: "Market", answer: { lat: 38.884316, lng: -76.995659 } },
-      { name: "9:30 Club", category: "Music Venue", answer: { lat: 38.917999, lng: -77.023739 } },
-      { name: "Gravelly Point", category: "Park", answer: { lat: 38.864456, lng: -77.043419 } }
-    ]
-  },
-
   "2026-05-19": {
     id: "001",
       locations: [
@@ -121,3 +72,158 @@ export const puzzlesByDate = {
     ]
   }
 };
+
+  "2026-05-25": {
+    id: "004",
+    locations: [
+     {
+      name: "YELLOW Georgetown",
+      category: "Cafe",
+      answer: { lat: 38.9095, lng: -77.06444 }
+    },
+    {
+      name: "Soleluna",
+      category: "Bakery",
+      answer: { lat: 38.92301, lng: -77.04217 }
+    },
+    {
+      name: "Banneker Recreation Center",
+      category: "Recreation Center",
+      answer: { lat: 38.92272, lng: -77.02231 }
+    },
+    {
+      name: "Hillwood Estate, Museum and Gardens",
+      category: "Museum",
+      answer: { lat: 38.94377, lng: -77.05267 }
+    },
+    {
+      name: "Hirshhorn Museum",
+      category: "Museum",
+      answer: { lat: 38.88757, lng: -77.02208 }
+    }
+  ]
+}
+
+  "2026-05-26": {
+    id: "005",
+  locations: [
+    {
+      name: "Albi",
+      category: "Restaurant",
+      answer: { lat: 38.88473, lng: -77.000267 }
+    },
+    {
+      name: "National Building Museum",
+      category: "Museum",
+      answer: { lat: 38.89813, lng: -77.01753 }
+    },
+    {
+      name: "Meridian Hill Park",
+      category: "Park",
+      answer: { lat: 38.921551, lng: -77.035762 }
+    },
+    {
+      name: "Black Cat",
+      category: "Music Venue",
+      answer: { lat: 38.91457, lng: -77.03195 }
+    },
+    {
+      name: "Dumbarton Oaks",
+      category: "Museum/Garden",
+      answer: { lat: 38.913338, lng: -77.063089 }
+    }
+  ]
+}
+
+  "2026-05-27": {
+    id: "006",
+    locations: [
+    {
+      name: "Le Diplomate",
+      category: "Restaurant",
+      answer: { lat: 38.91136, lng: -77.03195 }
+    },
+    {
+      name: "National Portrait Gallery",
+      category: "Museum",
+      answer: { lat: 38.897875, lng: -77.022898 }
+    },
+    {
+      name: "Yards Park",
+      category: "Park",
+      answer: { lat: 38.873452, lng: -77.001116 }
+    },
+    {
+      name: "Politics and Prose Bookstore",
+      category: "Bookstore",
+      answer: { lat: 38.95524, lng: -77.06952 }
+    },
+    {
+      name: "Folger Shakespeare Library",
+      category: "Library/Theater",
+      answer: { lat: 38.88982, lng: -77.00277 }
+    }
+  ]
+}
+
+  "2026-05-28": {
+    id: "007",
+  locations: [
+    {
+      name: "L'Ardente",
+      category: "Restaurant",
+      answer: { lat: 38.89943, lng: -77.01411 }
+    },
+    {
+      name: "National Cathedral",
+      category: "Cathedral",
+      answer: { lat: 38.93097, lng: -77.07096 }
+    },
+    {
+      name: "Hains Point",
+      category: "Park",
+      answer: { lat: 38.856840, lng: -77.022138 }
+    },
+    {
+      name: "Union Stage",
+      category: "Music Venue",
+      answer: { lat: 38.87853, lng: -77.02378 }
+    },
+    {
+      name: "The Mansion on O Street",
+      category: "Museum/Hotel",
+      answer: { lat: 38.90857, lng: -77.04585 }
+    }
+  ]
+}
+
+  "2026-05-29": {
+    id: "008",
+  locations: [
+    {
+      name: "Capital One Arena",
+      category: "Arena",
+      answer: { lat: 38.89778, lng: -77.020898 }
+    },
+    {
+      name: "Nationals Park",
+      category: "Stadium",
+      answer: { lat: 38.872672, lng: -77.007483 }
+    },
+    {
+      name: "Kenilworth Aquatic Gardens",
+      category: "Garden",
+      answer: { lat: 38.91183, lng: -76.94087 }
+    },
+    {
+      name: "Arena Stage",
+      category: "Theater",
+      answer: { lat: 38.87713, lng: -77.01987 }
+    },
+    {
+      name: "Oyster Oyster",
+      category: "Restaurant",
+      answer: { lat: 38.90922, lng: -77.02295 }
+    }
+  ]
+}
